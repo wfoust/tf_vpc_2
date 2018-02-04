2363300f6a2f5b65aa9125b93850eb7d5df4cdc7
Bill <foustwl@gmail.com>
2018-02-04 12:54:38 -0600

    First commit of VPC module

---
+++ interface.tf
@@ -0,0 +1,33 @@
+variable "name" {
+  description = "The name of the VPC."
+}
+
+variable "cidr" {
+  description = "The CIDR of the VPC."
+}
+
+variable "public_subnet" {
+  description = "The public subnet to create."
+}
+
+variable "enable_dns_hostnames" {
+  description = "Should be true if you want to use private DNS within the VPC"
+  default = true
+}
+
+variable "enable_dns_support" {
+  description = "Should be true if you want to use private DNS within the VPC"
+  default = true
+}
+
+output "public_subnet_id" {
+  value = "${aws_subnet.public.id}"
+}
+
+output "vpc_id" {
+  value = "${aws_vpc.tfb.id}"
+}
+
+output "cidr" {
+  value = "${aws_vpc.tfb.cidr_block}"
+}
